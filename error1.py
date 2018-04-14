6c6db64fcbda2caea525c35dc79f0b694b829123
stevencrack <32911023+stevencrack@users.noreply.github.com>
2018-04-14 12:02:17 -0500

    Add files via upload

---
+++ error1.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = np.loadtxt('datosrk1.txt')
+x = data[:,0]
+y = data[:,1]
+h = 0.1
+N = 3/h
+
+yr = np.exp(-x)
+
+e = np.abs((yr - y)/yr)*100
+
+plt.figure()
+plt.semilogy(x, e, label='Error')
+plt.savefig('error1.pdf')
